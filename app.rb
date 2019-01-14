df791ce790d732a95d1c335dcff6b38432652014
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2019-01-14 15:37:05 +0900

    edit token and config.ru

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ get '/' do
 end
 
 get '/callback' do
-  if params["hub.verify_token"] != 'EAAf9VlE5LSsBAHYtSZC3BuXnfWwG32BzWZB3oeIiRSVxG9mmFLMa3vGfdlxklQ9ZA7dB1aOI56woQoJ2ZAmWTBXVWU6nj1tbMpI5s9QPT40iZAZBad9keicRN2TSRn44bxSJDAJlHL09PxcjqvRSKkg1XZCwgjB8H9Awvmejq03gySnpvYqw4EL'
-    return 'Error, wrong validation token'
+  if params["hub.verify_token"] != 'foobarbaz'
+    return 'Error, wrong validation tokenz'
   end
     params["hub.challenge"]
 end
--- config.ru
+++ config.ru
@@ -2,4 +2,3 @@ require './app'
 run Sinatra::Application
 
 $stdout.sync = true
-run Sinatra::Application
