3794d59f60febd4ce575b714d40f008bff345110
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2019-01-14 16:30:04 +0900

    edit reply function

--- app.rb
+++ app.rb
@@ -16,19 +16,22 @@ get '/callback' do
     params["hub.challenge"]
 end
 
+# when receiving message
 post '/callback' do
   hash = JSON.parse(request.body.read)
-  message = hash["entry"][0]["messaging"][0] #entryの0個目のmessagingの0個目
-  sender = message["sender"]["id"] #上記で取得したmessage変数の中のsenderのid
-  text = message["message"]["text"]#上記で取得したmessage変数の中のmessageのtest
+  message = hash["entry"][0]["messaging"][0]
+  sender = message["sender"]["id"]
+
+  #  add message in text 
+text = "Search restaurants by your location and category. Say 'hungry!' "
   endpoint = "https://graph.facebook.com/v2.6/me/messages?access_token=" + "EAAf9VlE5LSsBAMZBnEDd6kQlnHzhDGE3XHQXHe8ZApLWmeC2JiHxOHk29JwU4AnpXvJHxkRFI1ALx3cZAutrc8ybFfLAbXONRENzgFTIukyZCrSO7LxhtLb09yRGyAZCd6CSHvjMZAPfuEl1CyPD5ZAjMqehga8cxm2AkD8AEMPHbnOuGkstrfB"
   content = {
     recipient: {id: sender},
     message: {text: text}
   }
-  request_body = content.to_json
+  request_body = content.to_json # convert to json
 
-  #オウム返しの返信をPOSTする（返す）
+  #reply by POST
   RestClient.post endpoint, request_body, content_type: :json, accept: :json
   status 201
   body ''
